17e6667c6fa948e4b3ccd582dea98e6c9596c2ea
Biniam <Biniamt23@yahoo.com>
2020-02-24 20:57:20 -0800

    interface created

---
+++ ToDoWebApp/ToDoWebApp/Models/Services/TaskService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoWebApp.Data;
+using ToDoWebApp.Models.Interfaces;
+
+namespace ToDoWebApp.Models.Services
+{
+    public class TaskService : ITaskManager
+    {
+        private StorageDbContext _context;
+
+        public TaskService(StorageDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateATaskAsync(ToDoList task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTaskAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ToDoList> GetTaskByIDAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ToDoList>> GetTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTaskAsync(ToDoList task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
