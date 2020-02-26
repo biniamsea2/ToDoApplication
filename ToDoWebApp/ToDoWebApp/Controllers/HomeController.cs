ed19acbe4e943d1f220a1d423108d164c79a7745
Biniam <Biniamt23@yahoo.com>
2020-02-25 22:39:52 -0800

    controller completed

--- ToDoWebApp/ToDoWebApp/Controllers/HomeController.cs
+++ ToDoWebApp/ToDoWebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,10 +18,131 @@ namespace ToDoWebApp.Controllers
             _context = task;
         }
 
+        #region Index
         public async Task <IActionResult> Index()
         {
             return View(await _context.GetTasksAsync());
         }
+        #endregion
+
+        #region Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Task,Priority,Completed,DueDate")] ToDoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.CreateATaskAsync(item);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+        #endregion
+
+        #region Details
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            ToDoList task = await _context.GetTaskByIDAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return View(task);
+        }
+        #endregion
+
+        #region Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.GetTaskByIDAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+        #endregion
+
+        #region Edit (Post)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Task,Priority,Completed,DueDate")] ToDoList item)
+        {
+            if (id != item.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _context.UpdateTaskAsync(item);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await TaskExists(item.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+        #endregion
+
+        #region Task Exists
+        private async Task<bool> TaskExists(int id)
+        {
+            ToDoList task = await _context.GetTaskByIDAsync(id);
+            if (task != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return NotFound();
+            }
+
+            var task = await _context.GetTaskByIDAsync(id);
+            return View(task);
+        }
+        #endregion
+
+        #region Confirm Deletion
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _context.DeleteTaskAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
 
         public IActionResult About()
         {
